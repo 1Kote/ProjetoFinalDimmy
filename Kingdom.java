cb569f208d22240d7859527148a38da63febf7e9
Sergiosmf <134345634+Sergiosmf@users.noreply.github.com>
2023-06-13 18:42:32 -0300

    Create Kingdom.java

---
+++ Kingdom.java
@@ -0,0 +1,37 @@
+public class Kingdom {
+	private String nameK;
+	
+	public Kingdom(String nameK) {
+		this.setNameK(nameK);
+	}
+
+	public String getNameK() {
+		return nameK;
+	}
+
+	public void setNameK(String nameK) {
+		this.nameK = nameK;
+	}
+	
+}
+
+public class HashMap {
+	
+	Kingdom UBU = new Kingdom("UBU");
+	Kingdom KLE = new Kingdom("KLE");
+	Kingdom PNE = new Kingdom("PNE");
+	Kingdom PGR = new Kingdom("PGR");
+	Kingdom PDO = new Kingdom("PDO");
+	Kingdom KLA = new Kingdom("KLA");
+	Kingdom GDS = new Kingdom("GDS");
+	Kingdom KOL = new Kingdom("KOL");
+	Kingdom DEF = new Kingdom("DEF");
+	Kingdom KKA = new Kingdom("KKA");
+	Kingdom ALE = new Kingdom("ALE");
+	Kingdom PKR = new Kingdom("PKR");
+	Kingdom VEM = new Kingdom("VEM");
+	Kingdom CSU = new Kingdom("CSU");
+	Kingdom PKS = new Kingdom("PKS");
+	Kingdom BUN = new Kingdom("BUN");
+	Kingdom NAR = new Kingdom("NAR");
+	
