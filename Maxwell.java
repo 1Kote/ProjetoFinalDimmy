410fc9f82f122bda0964f1bbf229cd784ae75b91
Enzo Guimarães Miguel <81970792+EnzoGui18@users.noreply.github.com>
2023-06-20 17:56:05 -0300

    Update Maxwell.java

--- Maxwell.java
+++ Maxwell.java
@@ -1,3 +1,4 @@
+//Classe do Personagem Maxwell
 public class Maxwell {
 	private String currentCity;
 	private int transportCoin;
@@ -44,12 +45,51 @@ public void setPowerthreshold(int powerthreshold) {
 	this.powerthreshold = powerthreshold;
 }
 
+	//metodo para realizar uma missão
 public void accomplishMission(Mission mission) {
-	//
+	if (transportCoin >= mission.getCoustMission()){
+		System.out.println("Missão Aceita!");
+
+		//Reduzir o numero de moedas de transporte de acordo com o custo da missão
+		transportCoin -= mission.getCoustMission();
+
+		//atulizar o limiar de poder de acordo com a missão
+		powerthreshold += mission.get.ThresholdIncrease();
+		System.out.printlln("Prêmio da missão recebido!")
+		
+		//especificidades da missão, caso tenha
+		//atualizar outros atributos
+		
+	} else{
+		System.out.println("Moedas insuficientes para realizar missão!")
+	}
 }
 
-public void exchangeCoinsL(boolean //add nome aceitar missao , boolean //add nome setado da distancia// ) {
-	//
+public void exchangeCoinsL(int amountCoins) {
+	if(transportCoin >= amountCoins){
+		// verificar se a distancia é menor que 3
+		boolean distanceThree = calcularDistance() < 3;
+		// verificar quantidade de moedas se é menor que 5
+		boolean CoinLowFive = transportCoin < 5;
+		if ((CoinLowFive && distanceThree) || (!CoinLowFive && !distanceThree)){
+		System.out.println("Você trocou " + amountCoins + " moedas por limiar na jóia.");
+
+		transportCoin -= amountCoins;
+		
+		if (distanceThree){
+			powerthreshold += 1;
+		} else {
+			powerthreshold -= 2;
+		}
+			System.out.println("Limiar de poder da jóia atualizado: " + limiarPoder);
+			//mais ações sobre a troca de moedas por poder
+		} else {
+			System.out.println("A quantidade de moedas e a distância não estão de acordo com as regras para trocar por limiar.")
+				
+		} else {
+			System.out.println("Você não tem moedas de transporte suficientes para realizar a troca.");
+		}
+	}
 }
                                                                              
 }
