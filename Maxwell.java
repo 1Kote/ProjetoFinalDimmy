b24e4529939a40b71c6f79046cb7964d5f12a275
Enzo Guimarães Miguel <81970792+EnzoGui18@users.noreply.github.com>
2023-06-19 23:59:05 -0300

    Create Maxwell.java

---
+++ Maxwell.java
@@ -0,0 +1,56 @@
+public class Maxwell {
+	private String currentCity;
+	private int transportCoin;
+	private int powerJoia;
+	private int powerthreshold;
+
+public Maxwell() {
+	currentCity = "00-UBU";
+	transportCoin = 3;
+	powerJoia = 0;
+	powerthreshold = 7;
+	
+}
+
+public String getCurrentCity() {
+	return currentCity;
+}
+
+public void setCurrentCity(String currentCity) {
+	this.currentCity = currentCity;
+}
+
+public int getPowerJoia() {
+	return powerJoia;
+}
+
+public void setPowerJoia(int powerJoia) {
+	this.powerJoia = powerJoia;
+}
+
+public int getTransportCoin() {
+	return transportCoin;
+}
+
+public void setTransportCoin(int transportCoin) {
+	this.transportCoin = transportCoin;
+}
+
+public int getPowerthreshold() {
+	return powerthreshold;
+}
+
+public void setPowerthreshold(int powerthreshold) {
+	this.powerthreshold = powerthreshold;
+}
+
+public void realizarMissao(Missao missao) {
+	//
+}
+
+public void trocarMoedasLimiar(boolean aceitaMissao, boolean distanciaMenorTres) {
+	//
+}
+                                                                             
+}
+	
